3c302b1b8c671af4a3b9bac1e2d47cacf5756f29
Peter Malúch <peter_maluch@hotmail.com>
2025-06-04 23:30:13 +0200

    8

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,8 +15,6 @@ Route::resource("/users",UsersController::class);
 Route::resource("/conferences",ConferenceController::class);
 Route::resource("/pages",PagesController::class);
 
-Route::post('/admin/removeEditor',[AdminController::class,'removeEditor']);
-
 Route::middleware('auth:api')->group(function(){
     Route::post('/logout', [AuthController::class,'logout']);
     Route::get("/profile/conferences/{id}",[ProfileController::class,"conferences"]);
@@ -28,5 +26,5 @@ Route::middleware(AdminMiddleware::class)->group(function(){
     Route::get('/admin/assignedEditors/{id}',[AdminController::class,'assignedEditors']);
     Route::get('/admin/searchUsers/{name}',[AdminController::class,'searchUsers']);
     Route::post('/admin/addEditor',[AdminController::class,'addEditor']);
-    
+    Route::post('/admin/removeEditor',[AdminController::class,'removeEditor']);
 });
